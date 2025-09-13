19cdaecf25676826b41133759d5ca9ea10b025bd
Sandeepan Ganguly <40802580+meetsandeepan@users.noreply.github.com>
2025-09-13 13:05:25 -0400

    Footer Nav Error

--- src/config/footerNavigation.ts
+++ src/config/footerNavigation.ts
@@ -2,106 +2,110 @@
 // ------------
 // Description: The footer navigation data for the website.
 export interface Logo {
-    src: string
-    alt: string
+	src: string
+	alt: string
+	text: string
 }
 
 export interface FooterAbout {
-    aboutText: string
-    logo: Logo
+	title: string
+	aboutText: string
+	logo: Logo
 }
 
 export interface SubCategory {
-    subCategory: string
-    subCategoryLink: string
+	subCategory: string
+	subCategoryLink: string
 }
 
 export interface FooterColumn {
-    category: string
-    subCategories: SubCategory[]
+	category: string
+	subCategories: SubCategory[]
 }
 
 export interface SubFooter {
-    copywriteText: string
+	copywriteText: string
 }
 
 export interface FooterData {
-    footerAbout: FooterAbout
-    footerColumns: FooterColumn[]
-    subFooter: SubFooter
+	footerAbout: FooterAbout
+	footerColumns: FooterColumn[]
+	subFooter: SubFooter
 }
 
 export const footerNavigationData: FooterData = {
-    footerAbout: {
-        aboutText:
-            'Rompus Health is a AI Clinical Decision Support Software built for oncology practices to deliver guideline concordant care and streamline prior authorization',
-        logo: {
-            src: '/logo.svg',
-            alt: 'Intelligent Prior Authorization Solution for Oncology'
-        }
-    },
-    footerColumns: [
-        {
-            category: 'Product',
-            subCategories: [
-                {
-                    subCategory: 'Features',
-                    subCategoryLink: '/features'
-                },
-                {
-                    subCategory: 'FAQ',
-                    subCategoryLink: '/faq'
-                },
-                {
-                    subCategory: 'Pricing',
-                    subCategoryLink: '/pricing'
-                },
-                {
-                    subCategory: 'Changelog',
-                    subCategoryLink: '/changelog'
-                },
-                {
-                    subCategory: 'Terms',
-                    subCategoryLink: '/terms'
-                }
-            ]
-        },
-        {
-            category: 'About us',
-            subCategories: [
-                {
-                    subCategory: 'About us',
-                    subCategoryLink: '/'
-                },
-                {
-                    subCategory: 'News',
-                    subCategoryLink: '/blog'
-                },
-                {
-                    subCategory: 'Careers',
-                    subCategoryLink: '/blog'
-                }
-            ]
-        },
-        {
-            category: 'Get in touch',
-            subCategories: [
-                {
-                    subCategory: 'Contact',
-                    subCategoryLink: '/contact'
-                },
-                {
-                    subCategory: 'Support',
-                    subCategoryLink: '/contact'
-                },
-                {
-                    subCategory: 'Join us',
-                    subCategoryLink: '/contact'
-                }
-            ]
-        }
-    ],
-    subFooter: {
-        copywriteText: '© Rompus Health 2024.'
-    }
+	footerAbout: {
+		title: 'Rompus Health',
+		aboutText:
+			'Rompus Health is a AI Clinical Decision Support Software built for oncology practices to deliver guideline concordant care and streamline prior authorization',
+		logo: {
+			src: '/logo.svg',
+			alt: 'End to End Prior Authorization for Oncology',
+			text: 'Rompus'
+		}
+	},
+	footerColumns: [
+		{
+			category: 'Product',
+			subCategories: [
+				{
+					subCategory: 'Features',
+					subCategoryLink: '/features'
+				},
+				{
+					subCategory: 'FAQ',
+					subCategoryLink: '/faq'
+				},
+				{
+					subCategory: 'Pricing',
+					subCategoryLink: '/pricing'
+				},
+				{
+					subCategory: 'Changelog',
+					subCategoryLink: '/changelog'
+				},
+				{
+					subCategory: 'Terms',
+					subCategoryLink: '/terms'
+				}
+			]
+		},
+		{
+			category: 'About us',
+			subCategories: [
+				{
+					subCategory: 'About us',
+					subCategoryLink: '/'
+				},
+				{
+					subCategory: 'News',
+					subCategoryLink: '/blog'
+				},
+				{
+					subCategory: 'Careers',
+					subCategoryLink: '/blog'
+				}
+			]
+		},
+		{
+			category: 'Get in touch',
+			subCategories: [
+				{
+					subCategory: 'Contact',
+					subCategoryLink: '/contact'
+				},
+				{
+					subCategory: 'Support',
+					subCategoryLink: '/contact'
+				},
+				{
+					subCategory: 'Join us',
+					subCategoryLink: '/contact'
+				}
+			]
+		}
+	],
+	subFooter: {
+		copywriteText: '© Rompus Health 2024.'
+	}
 }
