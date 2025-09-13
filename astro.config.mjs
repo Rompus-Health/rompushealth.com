0056c4217fb848bbf35b2817e32ff66696312d66
Sandeepan Ganguly <40802580+meetsandeepan@users.noreply.github.com>
2025-09-13 08:22:40 -0400

    linkedin + github social update, X redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app/",
+  site: "https://rompushealth.com",
   integrations: [
     tailwind(),
-    icon(),
+    icon({
+      collections: {
+        local: "src/icons",
+      },
+    }),
     sitemap(),
     partytown({
       config: {
